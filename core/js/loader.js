6c29b516e9520aa42a059867b6295e769081f8da
CastilloDelSol <50454721+CastilloDelSol@users.noreply.github.com>
2025-12-04 08:31:30 +0100

    Update loader.js

--- core/js/loader.js
+++ core/js/loader.js
@@ -1,30 +1,39 @@
 (async function() {
-    // 1. SCRIPT-PFAD SELBST ERKENNEN
-    const scriptEl = document.currentScript;
-    const scriptPath = scriptEl.src;              // absoluter URL-Pfad
-    const scriptDir = scriptPath.replace(/\/loader\.js$/, "");  
 
-    // 2. CORE-DIR ist parent:
-    const coreDir = scriptDir.replace(/\/js$/, "");
+    // 1. Loader Position (absolute URL)
+    const script = document.currentScript;
+    const loaderUrl = new URL(script.src);
+    const loaderDir = loaderUrl.pathname.replace(/\/loader\.js$/, "");
 
-    // 3. EVENT-PFAD herausfinden (dort wo index.html liegt)
-    const eventUrl = window.location.pathname;
-    const eventDir = eventUrl.replace(/\/index\.html$/, "");
+    // 2. Core dir
+    const coreDir = loaderDir.replace(/\/js$/, "");
+
+    // 3. Event dir (URL der Seite, wo index.html liegt)
+    const pageUrl = new URL(window.location.href);
+    const eventDir = pageUrl.pathname.replace(/\/index\.html$/, "");
 
     // 4. Template laden
-    const template = await fetch(coreDir + "/template.html").then(r => r.text());
+    const templateUrl = coreDir + "/template.html";
+    const template = await fetch(templateUrl).then(r => r.text());
 
-    // 5. EVENT CSS / JS Pfade setzen
-    const eventCss = eventDir + "/event.css";
-    const eventJs  = eventDir + "/event.js";
-
-    // 6. Ersetzen
+    // 5. Platzhalter ersetzen
     let html = template
         .replace(/{{EVENT_NAME}}/g, window.EVENT.name)
         .replace(/{{EVENT_YEAR}}/g, window.EVENT.year)
-        .replace("{{EVENT_CSS}}", `<link rel="stylesheet" href="${eventCss}">`)
-        .replace("{{EVENT_JS}}", `<script src="${eventJs}"></script>`);
 
-    // 7. HTML sauber einfügen
+        // Core includes (absolute korrekt)
+        .replace("{{CORE_CSS}}",
+            `<link rel="stylesheet" href="${coreDir}/css/style.css">`)
+        .replace("{{CORE_JS}}",
+            `<script src="${coreDir}/js/core.js"></script>`)
+
+        // Event includes (korrekt relativ zum Event)
+        .replace("{{EVENT_CSS}}",
+            `<link rel="stylesheet" href="${eventDir}/event.css">`)
+        .replace("{{EVENT_JS}}",
+            `<script src="${eventDir}/event.js"></script>`);
+
+    // 6. Template rendern
     document.body.innerHTML = html;
+
 })();
