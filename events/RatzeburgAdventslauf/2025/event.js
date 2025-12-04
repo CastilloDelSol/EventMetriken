ffbf5251925cb3c1c3ee6193e75e9746482d3f53
CastilloDelSol <50454721+CastilloDelSol@users.noreply.github.com>
2025-12-04 10:18:26 +0100

    Update event.js

--- events/RatzeburgAdventslauf/2025/event.js
+++ events/RatzeburgAdventslauf/2025/event.js
@@ -1,3 +1,6 @@
 // event.js
 
-/* empty */
+export async function loadEventSettings() {
+    const res = await fetch(`${window.EVENT.dir}/data/settings.json`);
+    window.EVENT.settings = await res.json();
+}
